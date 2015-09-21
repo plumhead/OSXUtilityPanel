34399a94561186c3df8917e7a13c83b3452eabab
Andy Calderbank <andy@plumhead-software.com>
2015-09-21 08:56:49 +0100

    Created blank application template via XCode

---
+++ OSXUtilityPanel/OSXUtilityPanel/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  OSXUtilityPanel
+//
+//  Created by Andrew Calderbank on 21/09/2015.
+//  Copyright © 2015 Andrew Calderbank. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ OSXUtilityPanel/OSXUtilityPanel/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  OSXUtilityPanel
+//
+//  Created by Andrew Calderbank on 21/09/2015.
+//  Copyright © 2015 Andrew Calderbank. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
